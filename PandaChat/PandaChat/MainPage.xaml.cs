1b90143d7fece96d7d76bfec942f42f994295885
Paweł Cyman <tazfelis@gmail.com>
2016-05-13 19:48:36 +0200

    Geoposition functionality.

--- PandaChat/PandaChat/MainPage.xaml.cs
+++ PandaChat/PandaChat/MainPage.xaml.cs
@@ -8,12 +8,13 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using PandaChat.Resources;
+using Windows.Devices.Geolocation;
 
 namespace PandaChat
 {
     public partial class MainPage : PhoneApplicationPage
     {
-
+        Geolocator geolocator;
         // Constructor
         public MainPage()
         {
@@ -30,8 +31,35 @@ namespace PandaChat
         {
             if (textBoxName.Text != "")
             {
+                if (!(bool)radioButtonLocation.IsChecked)
+                {
+                    textBoxName.Text = textBoxName.Text.Trim();
+                    NavigationService.Navigate(new Uri("/ChatPage.xaml?name=" + textBoxName.Text, UriKind.Relative));
+                }
+                else
+                {
+                    getPosition();
+                }
+            }
+        }
+
+        private async void getPosition()
+        {
+            try
+            {
+                //get position
+                geolocator = new Geolocator();
+                Geoposition geoposition = await geolocator.GetGeopositionAsync();
+                //send position to db
+                WebClient webclient = new WebClient();
+                webclient.DownloadStringAsync(new Uri("http://mayumu.bluequeen.tk/kawaiiApi.php?key=supersekretnyklucz&method=add&name="+textBoxName.Text+"&latitude="+geoposition.Coordinate.Latitude.ToString()+"&longitude="+geoposition.Coordinate.Longitude.ToString()));
+                //connect to chat
                 textBoxName.Text = textBoxName.Text.Trim();
                 NavigationService.Navigate(new Uri("/ChatPage.xaml?name=" + textBoxName.Text, UriKind.Relative));
+            }
+            catch
+            {
+                //GPS error
             }
         }
     }
